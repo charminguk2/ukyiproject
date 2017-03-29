d7a2f01e2c456da8b2d2e5b35c8afeacdd52ad74
charminguk2 <charm@UkYi>
2017-03-29 19:28:36 +0900

    데이터소스테스트,마이바티스테스트, 오라클JDBC연결테스트 완

---
+++ ukyi_project/src/test/java/uk0329/OracleConnectionTest.java
@@ -0,0 +1,26 @@
+package uk0329;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+//JDBC 연결테스트
+public class OracleConnectionTest {
+	private static final String Driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String Url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String User = "scott";
+	private static final String Pw = "tiger";
+	
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(Driver);
+		try {
+			Connection con = DriverManager.getConnection(Url,User,Pw);
+					System.out.println(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
